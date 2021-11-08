c6b75788a64d2e673f4f3f6dcb8b5df5d81b8bb0
Cristi Dalan <cristidalan21@gmail.com>
2021-11-08 18:44:51 +0200

    lab 2

---
+++ src/lab2/ch1/Fighter.java
@@ -0,0 +1,28 @@
+package lab2.ch1;
+
+public class Fighter {
+    private String name;
+    private int health;
+    private int damagePerAttack;
+
+    public Fighter(String name, int health, int damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamagePerAttack() {
+        return damagePerAttack;
+    }
+
+    public void attack(Fighter f){
+    }
+
+}
